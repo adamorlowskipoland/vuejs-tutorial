bd3aeefe42a3b347062dbf65b44ca8a4bb1068bd
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-19 22:12:24 +0200

    data binding

--- app.js
+++ app.js
@@ -2,7 +2,9 @@ new Vue({
   el:'#vue-app',
   data: {
     name: 'Shaun',
-    job: 'Ninja'
+    job: 'Ninja',
+    website: 'http://www.thenetninja.com',
+    websiteTag: '<a href="http://www.thenetninja.com">The Ninja Website</a>'
   },
   methods: {
     greet (time) {
