69e68c51492b1f92bbd83bd2e2f7eb9efd480a4f
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-19 22:48:11 +0200

    keyboard events and compiling couple of them

--- app.js
+++ app.js
@@ -1,23 +1,14 @@
 new Vue({
   el: '#vue-app',
   data: {
-    age: 25,
-    x: 0,
-    y: 0
+
   },
   methods: {
-    add (inc) {
-      this.age += inc;
+    logName () {
+      console.log('You entered your name');
     },
-    substract (dec) {
-      this.age -= dec;
-    },
-    updateXY (event) {
-      this.x = event.offsetX;
-      this.y = event.offsetY;
-    },
-    click () {
-      console.log('You clicked a link');
+    logAge () {
+      console.log('You entered your age');
     }
   }
 });
