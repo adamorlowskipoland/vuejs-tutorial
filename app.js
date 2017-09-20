2043f2762d28a7a92a2a263f8100cca89ffd7039
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-21 00:13:32 +0200

    conditionals v-if & v-show start

--- app.js
+++ app.js
@@ -1,18 +1,12 @@
 new Vue ({
   el: '#vue-app',
   data: {
-    available: false,
-    nearby: false
+
   },
   methods: {
 
   },
   computed: {
-    compClasses () {
-      return {
-        available: this.available,
-        nearby: this.nearby
-      }
-    }
+
   }
 });
