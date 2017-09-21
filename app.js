d8bf5ecb312fed0776ba2c980cc7f750758afa60
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-21 23:01:52 +0200

    create simple component

--- app.js
+++ app.js
@@ -1,7 +1,21 @@
-const one = new Vue({
+Vue.component('greeting', {
+  template:'<p>Hey there, I am {{ name }}.<button @click="changeName">Change name</button></p>',
+  data () {
+    return {
+      name: 'Yoshi'
+    }
+  },
+  methods: {
+    changeName () {
+      this.name = 'Mario';
+    }
+  }
+});
+
+new Vue({
   el: '#vue-app-one'
 });
 
-const two = new Vue({
+new Vue({
   el: '#vue-app-two'
 });
