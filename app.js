1053055d63bca34a9f4eeda7929e35fcb38e0af2
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-21 22:44:25 +0200

    working with 2 vue instances

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
-new Vue({
-  el: '#vue-app',
+const one = new Vue({
+  el: '#vue-app-one',
   data: {
-
+    title: 'vue app one'
   },
   methods: {
-
+    console () {
+      console.log(this.title);
+    }
   },
   computed: {
-
+    greet () {
+      return 'Hello from app one :)'
+    }
   }
 });
+
+const two = new Vue({
+  el: '#vue-app-two',
+  data: {
+    title: 'vue app two'
+  },
+  methods: {
+    console () {
+      console.log(this.title);
+    },
+    changeTitle (givenData) {
+      one.title = givenData;
+    }
+  },
+  computed: {
+    greet () {
+      return 'Yo, dudes, this is app 2 speaking to ya :)'
+    }
+  }
+});
+
+two.title = 'Changed from outside';
