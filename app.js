930d2e3f7b6d4caa5adb85c91a7dd6579818155a
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-21 22:30:21 +0200

    multiple Vue Instances

--- app.js
+++ app.js
@@ -1,22 +1,12 @@
 new Vue({
   el: '#vue-app',
   data: {
-    health: 100,
-    ended: false
+
   },
   methods: {
-    punch () {
-      this.health -= 10;
-      if (this.health <= 0) {
-        this.ended = true;
-      }
-      console.log(this.health)
-    },
-    restart () {
-      this.health = 100;
-      this.ended = false;
-      console.log(this.health)
-    }
+
   },
-  computed: {}
+  computed: {
+
+  }
 });
