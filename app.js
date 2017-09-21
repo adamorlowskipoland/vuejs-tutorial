01ad62b9dacb67bc37932f4e815627f0382e3398
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-21 22:49:00 +0200

    components intro start

--- app.js
+++ app.js
@@ -1,38 +1,7 @@
 const one = new Vue({
-  el: '#vue-app-one',
-  data: {
-    title: 'vue app one'
-  },
-  methods: {
-    console () {
-      console.log(this.title);
-    }
-  },
-  computed: {
-    greet () {
-      return 'Hello from app one :)'
-    }
-  }
+  el: '#vue-app-one'
 });
 
 const two = new Vue({
-  el: '#vue-app-two',
-  data: {
-    title: 'vue app two'
-  },
-  methods: {
-    console () {
-      console.log(this.title);
-    },
-    changeTitle (givenData) {
-      one.title = givenData;
-    }
-  },
-  computed: {
-    greet () {
-      return 'Yo, dudes, this is app 2 speaking to ya :)'
-    }
-  }
+  el: '#vue-app-two'
 });
-
-two.title = 'Changed from outside';
