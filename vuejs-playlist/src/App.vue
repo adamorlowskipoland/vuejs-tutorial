02dc1854a1f563ec0750b30de4c2cf9a519b31e7
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-23 16:25:05 +0200

    prepare for slots tutorial

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -7,33 +7,19 @@
 </template>
 
 <script>
-  import AppHeader from './components/header.vue'
-  import AppFooter from './components/footer.vue'
-  import Ninjas from './components/ninjas.vue'
+  import FormHelper from './components/formHelper.vue'
 
   export default {
     components: {
-      AppHeader,
-      Ninjas,
-      AppFooter
+      FormHelper
     },
     data () {
       return {
-        ninjas: [
-          {name: 'Ryu', speciality: 'Vue Components', show: false},
-          {name: 'Crystal', speciality: 'HTML Wizardry', show: false},
-          {name: 'Hitoshi', speciality: 'Click Events', show: false},
-          {name: 'Tango', speciality: 'Conditionals', show: false},
-          {name: 'Kami', speciality: 'Webpack', show: false},
-          {name: 'Yoshi', speciality: 'Data Diggin', show: false}
-        ],
-        title: 'Vue Ninjas'
+
       }
     },
     methods: {
-      updateTitle (updatedTitle) {
-        this.title = updatedTitle;
-      }
+
     }
   }
 </script>
