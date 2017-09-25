c63238581d58be438a4c4a98f475cbcd35417e1f
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-25 21:19:41 +0200

    dynamic components start

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,38 +1,19 @@
 <template>
   <div>
-    <form-helper>
-      <div slot="form-header">
-        <h3>
-          This is the title of the form
-        </h3>
-        <p>
-          Information about form
-        </p>
-      </div>
-      <div slot="form-fields">
-        <input type="text" placeholder="name" required>
-        <input type="password" placeholder="password" required>
-      </div>
-      <div slot="form-controls">
-        <button v-on:click="handleSubmit">
-          Submit
-        </button>
-      </div>
-    </form-helper>
+
   </div>
 </template>
 
 <script>
-  import FormHelper from './components/formHelper.vue'
+  import FormOne from './components/formHelper.vue'
+  import FormTwo from './components/formHelper.vue'
 
   export default {
     components: {
-      FormHelper
+      FormOne,
+      FormTwo
     },
     data() {
-      return {
-        title: 'I am a dynamic slots title'
-      }
     },
     methods: {}
   }
