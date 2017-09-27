bd797f55d5604844b1959e80ad70d1aae0a30736
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-28 00:09:35 +0200

    prepared fields and style them

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,19 +1,15 @@
 <template>
   <div>
-
+    <add-blog></add-blog>
   </div>
 </template>
 
 <script>
-
+  import addBlog from './components/addBlog.vue'
 
   export default {
-
-    data() {
-      return {
-
-      }
-    },
-    methods: {}
+    components: {
+      addBlog
+    }
   }
 </script>
