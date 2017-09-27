fb57cbdb8fd8cf84db0e47259828139726789739
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-27 23:55:33 +0200

    prepare files for building blog

--- vuejs-playlist/src/App.vue
+++ vuejs-playlist/src/App.vue
@@ -1,25 +1,17 @@
 <template>
   <div>
-    <keep-alive>  <!--  store data while changing components  -->
-      <component v-bind:is="component"></component>
-    </keep-alive>
-    <button v-on:click="component = 'form-one'">Show form One</button>
-    <button v-on:click="component = 'form-two'">Show form Two</button>
+
   </div>
 </template>
 
 <script>
-  import FormOne from './components/form-one.vue'
-  import FormTwo from './components/form-two.vue'
+
 
   export default {
-    components: {
-      FormOne,
-      FormTwo
-    },
+
     data() {
       return {
-        component: FormOne
+
       }
     },
     methods: {}
