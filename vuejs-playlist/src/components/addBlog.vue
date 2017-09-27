db36c2e69387c92120df9ec0dc59c30f47ec625e
Adam Orlowski <adamorlowskipoland@icloud.com>
2017-09-28 00:14:51 +0200

    bind data by v-model.lazy

--- vuejs-playlist/src/components/addBlog.vue
+++ vuejs-playlist/src/components/addBlog.vue
@@ -3,22 +3,28 @@
     <h2>Add a New Blog Post</h2>
     <form action="">
       <label>Blog Title:</label>
-      <input type="text" required>
+      <input type="text" v-model.lazy="blog.title" required>
       <label>Blog Content</label>
-      <textarea></textarea>
+      <textarea v-model.lazy="blog.content"></textarea>
     </form>
     <div id="preview">
       <h3>Preview Blog</h3>
-      <p>Blog title:</p>
+      <p>Blog title: {{ blog.title }}</p>
       <p>Blog content:</p>
+      <p>{{ blog.content }}</p>
     </div>
   </div>
 </template>
 
 <script>
   export default {
-    data() {
-      return {}
+    data () {
+      return {
+        blog: {
+          title: '',
+          content: ''
+        }
+      }
     },
     methods: {}
   }
